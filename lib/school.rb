fac40826acbfe66b65c616b0d2aac019dc0509eb
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-23 16:19:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,9 @@ class School
   end 
   
   def add_student(student_name, grade)
-   
+   roster[grade] = [ ]
+   roster[grade] << "student_name"
+   roster => {"grade"=>["student_name"]
  end
   
 
