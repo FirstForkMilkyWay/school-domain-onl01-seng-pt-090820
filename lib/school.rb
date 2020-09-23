efbec644462a862759778642ba93565b3b2dcffb
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-23 15:25:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+
+class School 
+  
+  attr_accessor :name_of_school
+  
+  def initialize(name_of_school, roster)
+    @name_of_school = name_of_school
+    @roster = roster 
+    roster = hash.new
+  end 
+  
+  def roster 
+    hash = {}
+    return hash 
+  end 
+  
+  
+end 
